6c1ab3f4f1fcd453f78fc945605c4ebf22995f40
Shaun Pour <scpour@gmail.com>
2022-12-31 23:28:48 -0500

    Add files via upload

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
@@ -14,13 +14,9 @@ public class PlayerScript : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
         if (Input.GetKey(KeyCode.Space))
-        {
             body.velocity = new Vector2(body.velocity.x, speed);
-        }
-
-        //Set speed to 0 if position is equal to -8.820085 aka almost to the far left edge of the screen
     }
 }
